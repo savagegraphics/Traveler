25a80746c5f31f43b7578449c706737d81e23f38
olawaleraji <savagedotjsx@gmail.com>
2024-04-02 08:17:04 -0700

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ import Footer from './components/Footer'
 
 export default function Home () {
   return (
-    <div className=''>
+    <div className='bg-white'>
       <Hero />
       <Card />
       <Banner />
