ba5e2dde027bd4ca79a490c822a33cdfe6da0804
olawaleraji <savagedotjsx@gmail.com>
2024-04-02 08:09:21 -0700

    first commit

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -52,7 +52,11 @@ function Foot () {
         </div>
         <Footer.Divider />
         <div className='w-full sm:flex sm:items-center sm:justify-between'>
-          <Footer.Copyright href='#' by='TrekTopia™' year={currentYear} />
+          <Footer.Copyright
+            href='#'
+            by='TrekTopia™'
+            year={parseInt(currentYear)}
+          />
           <div className='mt-4 flex space-x-6 sm:mt-0 sm:justify-center'>
             <Footer.Icon href='#' icon={BsFacebook} />
             <Footer.Icon href='#' icon={BsInstagram} />
